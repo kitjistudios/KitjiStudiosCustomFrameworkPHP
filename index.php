12177511e776aa338ddbcbc6704e9d4165d34e4c
Shayne Marshall <smarshall@kitjistudios.com>
2018-03-15 13:37:42 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Hello';
+        ?>
+    </body>
+</html>
